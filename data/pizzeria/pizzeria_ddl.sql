3d8cd31cb8ca5f3bf2ef42b95253ed9b86c216ff
Denis Magda <magda7817@gmail.com>
2025-01-30 21:12:29 -0500

    updated dataset for chapter 5

--- data/pizzeria/pizzeria_ddl.sql
+++ data/pizzeria/pizzeria_ddl.sql
@@ -4,5 +4,6 @@ CREATE TABLE pizzeria.order_items (
     order_id INT NOT NULL,
     order_item_id INT NOT NULL,
     pizza JSONB NOT NULL,
+    price numeric(5,2) NOT NULL,
     PRIMARY KEY (order_id, order_item_id)
 );
